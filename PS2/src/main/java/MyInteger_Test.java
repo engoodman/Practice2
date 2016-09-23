33172fc2f3f7531b978dbcd64f5786900005842d
engoodman <Emma@Emma>
2016-09-23 00:50:41 -0400

    Practice set 2

---
+++ PS2/src/main/java/MyInteger_Test.java
@@ -0,0 +1,41 @@
+import static org.junit.Assert.*;
+
+import java.util.Collection;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import base.MyInteger;
+
+
+
+public class MyInteger_Test {
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		assertTrue(1==1);
+		
+		
+	}
+
+}
